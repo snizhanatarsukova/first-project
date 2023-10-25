aa3c234c0c7a1aad4d8adfd7b07eb455dcde92f6
snizhanatarsukova <147998302+snizhanatarsukova@users.noreply.github.com>
2023-10-25 18:31:17 -0400

    Update buble_sort.py

--- buble_sort.py
+++ buble_sort.py
@@ -1,13 +1,10 @@
 def bubbleSort(arr):
     n = len(arr)
-    # optimize code, so if the array is already sorted, it doesn't need
-    # to go through the entire process
+   
     swapped = False
     # Traverse through all array elements
     for i in range(n-1):
-        # range(n) also work but outer loop will
-        # repeat one time more than needed.
-        # Last i elements are already in place
+        
         for j in range(0, n-i-1):
  
             # traverse the array from 0 to n-i-1
