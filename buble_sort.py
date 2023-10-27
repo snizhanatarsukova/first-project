61397433682373df0331a08621ce1e39549f5ca7
snizhanatarsukova <147998302+snizhanatarsukova@users.noreply.github.com>
2023-10-27 15:11:59 -0400

    Update buble_sort.py

--- buble_sort.py
+++ buble_sort.py
@@ -6,21 +6,13 @@ def bubbleSort(arr):
     for i in range(n-1):
         
         for j in range(0, n-i-1):
- 
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
             if arr[j] > arr[j + 1]:
                 swapped = True
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
          
         if not swapped:
-            # if we haven't needed to make a single swap, we 
-            # can just exit the main loop.
             return
  
- 
-# Driver code to test above
 arr = [64, 34, 25, 12, 22, 11, 90]
  
 bubbleSort(arr)
@@ -28,3 +20,4 @@ bubbleSort(arr)
 print("Sorted array is:")
 for i in range(len(arr)):
     print("% d" % arr[i], end=" ")
+print("Execution finished")
