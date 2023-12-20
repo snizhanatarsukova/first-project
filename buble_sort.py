b0b5e1f5993925970bd154981a47b3b3b54b0acb
snizhanatarsukova <147998302+snizhanatarsukova@users.noreply.github.com>
2023-12-20 18:32:19 -0500

    Update buble_sort.py

--- buble_sort.py
+++ buble_sort.py
@@ -15,8 +15,5 @@ def bubbleSort(arr):
 arr = [64, 34, 25, 12, 22, 11, 90]
  
 bubbleSort(arr)
- 
-print("Sorted array is:")
 for i in range(len(arr)):
     print("% d" % arr[i], end=" ")
-print("Execution finished")
