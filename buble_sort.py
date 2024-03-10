a55d0c7930e4c9c8428f140cabad0dc05809a7ba
snizhanatarsukova <147998302+snizhanatarsukova@users.noreply.github.com>
2024-03-10 14:51:47 -0400

    Update buble_sort.py

--- buble_sort.py
+++ buble_sort.py
@@ -1,16 +1,4 @@
-def bubbleSort(arr):
-    n = len(arr)
-   
-    swapped = False # should be false always
-    for i in range(n-1):
-        
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-         
-        if not swapped:
-            return
+
 
 for j in range(0, n-i-1):
             if arr[j] > arr[j + 1]:
