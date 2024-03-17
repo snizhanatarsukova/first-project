178f94a1982172d5e4631b289b9692cf975cdc2d
snizhanatarsukova <147998302+snizhanatarsukova@users.noreply.github.com>
2024-03-16 20:54:47 -0400

    Update buble_sort.py

--- buble_sort.py
+++ buble_sort.py
@@ -8,37 +8,7 @@ for j in range(0, n-i-1):
         if not swapped:
             return
 
-for j in range(0, n-i-1):
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-         
-        if not swapped:
-            return
 
-for j in range(0, n-i-1):
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-         
-        if not swapped:
-            return
-
-for j in range(0, n-i-1):
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-         
-        if not swapped:
-            return
-
-for j in range(0, n-i-1):
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-         
-        if not swapped:
-            return
  
 arr = [64]
 bubbleSort(arr)
